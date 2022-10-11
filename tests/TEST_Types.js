8cd3a7deccde61544fc7e7c3c5dc79f2163a5bf2
RustyNova <rusty.nova.jsb@gmail.com>
2022-10-11 16:08:47 +0200

    Test Types
    Took 7 minutes

---
+++ tests/TEST_Types.js
@@ -0,0 +1,25 @@
+export const TEST_Types = {
+    fire: {
+        id: 1,
+        name: "fire",
+        strongAgainst: [3],
+        weakAgainst: [2],
+        immune: []
+    },
+
+    water: {
+        id: 2,
+        name: "water",
+        strongAgainst: [1],
+        weakAgainst: [3],
+        immune: []
+    },
+
+    plant: {
+        id: 3,
+        name: "plant",
+        strongAgainst: [2],
+        weakAgainst: [1],
+        immune: []
+    }
+}
