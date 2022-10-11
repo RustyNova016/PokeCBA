af499d21c603b22e434a534c1927bc809f3035da
RustyNova <rusty.nova.jsb@gmail.com>
2022-10-11 14:24:59 +0200

    Teams
    Took 2 minutes

---
+++ classes/Teams.js
@@ -0,0 +1,11 @@
+export class Teams {
+    id_player;
+    name;
+    pokemon_team;
+
+    constructor(id_player, name, pokemon_team) {
+        this.id_player = id_player;
+        this.name = name;
+        this.pokemon_team = pokemon_team;
+    }
+}
