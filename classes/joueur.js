87158ff3508e201ec07717025600342156c1c7c0
RustyNova <rusty.nova.jsb@gmail.com>
2022-10-11 10:18:57 +0200

    Constructeur
    Took 5 minutes

--- classes/joueur.js
+++ classes/joueur.js
@@ -5,4 +5,13 @@ export class Joueur {
     xp = 0;
     gold = 0;
     img = [];
+
+    constructor(pseudo, mail, mdp, xp, gold, img) {
+        this.pseudo = pseudo;
+        this.mail = mail;
+        this.mdp = mdp;
+        this.xp = xp;
+        this.gold = gold;
+        this.img = img;
+    }
 }
--- classes/pokemon.js
+++ classes/pokemon.js
@@ -5,5 +5,15 @@ export class Pokemon {
     defense = 0;
     vitesse = 0;
     img = [];
+
+
+    constructor(nom, PV, attaque, defense, vitesse, img) {
+        this.nom = nom;
+        this.PV = PV;
+        this.attaque = attaque;
+        this.defense = defense;
+        this.vitesse = vitesse;
+        this.img = img;
+    }
 }
 
