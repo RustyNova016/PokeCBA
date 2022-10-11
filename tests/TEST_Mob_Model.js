b9df5fbc3dae6fc86535f5f7f006b3108d72692e
RustyNova <rusty.nova.jsb@gmail.com>
2022-10-11 16:18:34 +0200

    Mob models
    Took 5 minutes

---
+++ tests/TEST_Mob_Model.js
@@ -0,0 +1,11 @@
+export const TEST_Mob_Model = {
+    babasaure: {
+        idMobModel: 1,
+        HP: 21,
+        attack: 21,
+        defense: 21,
+        speed: 21,
+        img: ["img1"],
+        abilities: []
+    }
+}
