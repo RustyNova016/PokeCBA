d02488a245e93f8e8f49a7b971cba739eb325f7b
QuentinTavernier <115533028+QuentinTavernier@users.noreply.github.com>
2022-10-20 11:15:08 +0200

    fin test

--- components/HealthBar.js
+++ components/HealthBar.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from "react";
 import * as Progress from "react-native-progress";
 
@@ -15,21 +14,3 @@ const HealthBar = (props) => {
 };
 
 export default HealthBar;
-=======
-import React from "react";
-import * as Progress from "react-native-progress";
-
-const HealthBar = (props) => {
-  return (
-    <Progress.Bar
-      style={props.styleHealthBar}
-      progress={props.progressHealthBar}
-      color={props.colorHealthBar}
-      width={props.widthHealthBar}
-      height={props.heigthHealthBar}
-    />
-  );
-};
-
-export default HealthBar;
->>>>>>> Stashed changes
--- vue/game/defaite.js
+++ vue/game/defaite.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React, { useState, useEffect, useRef } from "react";
 import { Text, Animated, View, StyleSheet, Image } from "react-native";
 
@@ -50,56 +49,3 @@ const styles = new StyleSheet.create({
     resizeMode: "contain",
   },
 });
-=======
-import React, { useState, useEffect, useRef } from "react";
-import { Text, Animated, View, StyleSheet, Image } from "react-native";
-
-export default function defaite() {
-  const [imgDEFAITE] = useState(require("../../images/imgDEFAITE.png"));
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 10000,
-    }).start();
-  }, [fadeAnim]);
-
-  return (
-    <View style={styles.container}>
-      <Animated.View
-        style={{
-          opacity: fadeAnim,
-        }}
-      >
-        <View>
-          <Image style={styles.img} source={imgDEFAITE} />
-          <Text style={styles.text}>DEFAITE :(</Text>
-        </View>
-      </Animated.View>
-    </View>
-  );
-}
-
-// CSS
-const styles = new StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#1F0F42",
-    justifyContent: "center",
-  },
-
-  text: {
-    marginTop: 30,
-    fontSize: 60,
-    textAlign: "center",
-    color: "#FFCC03",
-  },
-
-  img: {
-    width: "100%",
-    height: 400,
-    resizeMode: "contain",
-  },
-});
->>>>>>> Stashed changes
