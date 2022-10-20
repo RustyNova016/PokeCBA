45bef4d97244c59300f80f33112a76cd31fb12d3
QuentinTavernier <115533028+QuentinTavernier@users.noreply.github.com>
2022-10-20 11:09:00 +0200

    Systeme de combat
    V1

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import combatSysteme from "./vue/game/combatSysteme";
@@ -36,42 +35,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-=======
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import combatSysteme from "./vue/game/combatSysteme";
-import victoire from "./vue/game/victoire";
-import defaite from "./vue/game/defaite";
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="combatSysteme">
-        <Stack.Screen
-          name="combatSysteme"
-          component={combatSysteme}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="victoire"
-          component={victoire}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="defaite"
-          component={defaite}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
->>>>>>> Stashed changes
