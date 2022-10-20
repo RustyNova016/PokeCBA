b0b163bc1ef93c2dcde5a824c7cb54ebfa3c93f2
QuentinTavernier <115533028+QuentinTavernier@users.noreply.github.com>
2022-10-20 10:02:21 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,2 +1,37 @@
-// Use next.js page for the mobile app
-export { default } from './pages'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import combatSysteme from "./vue/game/combatSysteme";
+import victoire from "./vue/game/victoire";
+import defaite from "./vue/game/defaite";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="combatSysteme">
+        <Stack.Screen
+          name="combatSysteme"
+          component={combatSysteme}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="victoire"
+          component={victoire}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="defaite"
+          component={defaite}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
